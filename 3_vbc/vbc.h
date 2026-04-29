45837884f3e37d27802efd439b5aaf9b22895048
Maryam Adam <maryaada@lab2r3s14.42abudhabi.ae>
2026-04-29 13:50:34 +0400

    all three variants

---
+++ 3_vbc/vbc.h
@@ -0,0 +1,21 @@
+#ifndef VBC_H
+#define VBC_H
+
+
+#include <stdio.h>
+#include <stdlib.h>    // MODIFIED: use <stdlib.h> for calloc/free instead of <malloc.h>
+#include <ctype.h>
+
+typedef struct node {
+    enum {
+        ADD,
+        MULTI,
+        VAL
+    }   type;
+    int val;
+    struct node *l;
+    struct node *r;
+}   node;
+
+
+#endif
